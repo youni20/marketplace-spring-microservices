771434f92210e45d15389996b5d509f0cd025a40
Youni <youni@example.com>
2025-01-07 12:00:00 +0100

    feat: implement Spring Cloud Config Server

---
+++ config-server/src/main/java/com/ecommerce/config/ConfigApplication.java
@@ -0,0 +1,17 @@
+package com.ecommerce.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableConfigServer
+@EnableDiscoveryClient
+public class ConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigApplication.class, args);
+    }
+
+}
